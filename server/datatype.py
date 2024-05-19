be5c1838f05ca36d9213b1a0392160e373ae7027
qwerty121938 <jimjim951221@gmail.com>
2024-05-19 22:52:21 +0800

    new version

--- server/datatype.py
+++
@@ -1,8 +0,0 @@
-
-class Datatype():
-    def __init__(self):
-        self.client_socket = ''
-        self.client_address = ''
-        self.data_list = []
-        self.mission_list = []
-        self.thread_list = []
--- server/test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-arr = [5, 4, 3, 69, 11, 87]
-
-
-for i in arr:
-    
-    arr.remove(i)
-    print(arr)
